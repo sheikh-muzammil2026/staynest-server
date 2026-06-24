877c36eac7a8c169a7e90a66826cb398f5a313b4
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-06-24 21:50:45 +0600

    Comment out Express DB ping

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    await client.connect();
+    // await client.connect();
     const database = client.db("StayNest");
     const propertiesCollection = database.collection("properties");
     const reviewsCollection = database.collection("reviews");
@@ -367,7 +367,7 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
